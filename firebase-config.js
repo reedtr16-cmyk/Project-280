a85e6f99abd3e54412c7d6ad122ecc3a6072572e
reedtr16-cmyk <reedtr16@gmail.com>
2026-08-14 07:48:55 +1000

    Delete firebase-config.js

--- firebase-config.js
+++
@@ -1,9 +0,0 @@
-// Firebase config for the "chart-library" project.
-window.FIREBASE_CONFIG = {
-  apiKey: "AIzaSyCqJgKtT4X3_uG8vHPYvRwLlLdYkF7Nk8o",
-  authDomain: "project-280-4e17a.firebaseapp.com",
-  projectId: "project-280-4e17a",
-  storageBucket: "project-280-4e17a.firebasestorage.app",
-  messagingSenderId: "985146016085",
-  appId: "1:985146016085:web:4c39946336fa234851afdc",
-};
